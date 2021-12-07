eca89b2fcc1e408d682b19075aeb6b207619a0ab
innieminnie <rkddlsl1001@gmail.com>
2021-12-07 15:51:16 +0900

    feat: Measurement 구조체 생성

---
+++ Workout/Models/Measurement.swift
@@ -0,0 +1,14 @@
+//
+//  Measurement.swift
+//  Workout
+//
+//  Created by 강인희 on 2021/12/06.
+//
+
+import Foundation
+
+enum Measurement: String, CaseIterable {
+  case weightAndCount = "무게 & 횟수"
+  case count = "횟수"
+  case weight = "무게"
+}
---
+++ Workout/Models/Workout.swift
@@ -0,0 +1,8 @@
+//
+//  WorkOut.swift
+//  Workout
+//
+//  Created by 강인희 on 2021/12/06.
+//
+
+import Foundation
