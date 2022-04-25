de8ba854aedcc6238fe92cc6e8f865729d4550eb
innieminnie <rkddlsl1001@gmail.com>
2022-04-25 16:01:50 +0900

    feat: RoutineSelectionViewController의 tableView 구성

--- Workout/ViewControllers/RoutineSelectionViewController.swift
+++ Workout/ViewControllers/RoutineSelectionViewController.swift
@@ -8,22 +8,56 @@
 import UIKit
 
 class RoutineSelectionViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+  private let workoutListTableView: UITableView = {
+    let tableView = UITableView()
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    
+    return tableView
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    view.addSubview(workoutListTableView)
+    setUpListTableView()
+    setUpLayout()
+  }
+  
+  private func setUpLayout() {
+    NSLayoutConstraint.activate([
+      workoutListTableView.topAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.topAnchor, constant: 20),
+      workoutListTableView.leadingAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+      workoutListTableView.trailingAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+      workoutListTableView.bottomAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+    ])
+  }
+}
+extension RoutineSelectionViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return workoutManager.numberOfWorkoutList()
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: WorkoutTableViewCell.identifier, for: indexPath) as? WorkoutTableViewCell else {
+      return UITableViewCell()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    let workout = workoutManager.workout(at: indexPath.row)
+    cell.setUp(with: workout)
+    
+    return cell
+  }
+}
+extension RoutineSelectionViewController: UITableViewDelegate {
+  
+}
+extension RoutineSelectionViewController {
+  private func setUpListTableView() {
+    workoutListTableView.dataSource = self
+    workoutListTableView.delegate = self
+    
+    let nib = UINib(nibName: "WorkoutTableViewCell", bundle: nil)
+    self.workoutListTableView.register(nib, forCellReuseIdentifier: WorkoutTableViewCell.identifier)
+    self.workoutListTableView.separatorStyle = .none
+  }
 }
