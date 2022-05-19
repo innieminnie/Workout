0f32f40bead9b12c37487006b5eb8bca5cb5acc6
innieminnie <rkddlsl1001@gmail.com>
2022-05-19 15:05:23 +0900

    feat: SetConfigurationView 생성

---
+++ Workout/Views/SetConfigurationView.swift
@@ -0,0 +1,19 @@
+//
+//  SetConfigurationView.swift
+//  Workout
+//
+//  Created by 강인희 on 2022/05/19.
+//
+
+import UIKit
+
+class SetConfigurationView: UIView {
+  
+  init() {
+    super.init(frame: .zero)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
