fd806a52fcf3bd46611af34070f8352465059f96
innieminnie <rkddlsl1001@gmail.com>
2022-06-21 10:18:49 +0900

    feat: RoutineManager 타입 생성

---
+++ Workout/Models/RoutineManager.swift
@@ -0,0 +1,16 @@
+//
+//  RoutineManager.swift
+//  Workout
+//
+//  Created by 강인희 on 2022/06/21.
+//
+
+import Foundation
+
+class RoutineManager {
+  static let shared = RoutineManager()
+  
+  private init() { }
+}
+
+let routineManager = RoutineManager.shared
