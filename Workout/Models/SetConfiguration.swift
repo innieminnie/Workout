b37f0e62e68cd1e3bd14ab11e07ef6676c39d666
innieminnie <rkddlsl1001@gmail.com>
2022-07-13 12:18:00 +0900

    fix: totalSum 화면전환시 초기화되는 문제 수정

--- Workout/Models/SetConfiguration.swift
+++ Workout/Models/SetConfiguration.swift
@@ -8,25 +8,33 @@
 import Foundation
 
 class SetConfiguration {
-  var isDone: Bool
   var weight: Int
   var count: Int
   
   init() {
-    self.isDone = false
     self.weight = 0
     self.count = 0
   }
   
   init(weight w: Int) {
-    self.isDone = false
     self.weight = w
     self.count = 0
   }
   
   init(count c: Int) {
-    self.isDone = false
     self.weight = 0
     self.count = c
+  }
+  
+  func updateWeight(with w: Int) {
+    self.weight = w
+  }
+  
+  func updateCount(with c: Int) {
+    self.count = c
+  }
+  
+  func weightTimesCount() -> Int {
+    return self.weight * self.count
   }
 }
