ebbfd36cb21e62bbb800140c6f2ae938c090b333
innieminnie <rkddlsl1001@gmail.com>
2022-07-29 11:51:30 +0900

    refactor: HomeVC의 plannedWorkouts 제거

--- Workout/Models/RoutineManager.swift
+++ Workout/Models/RoutineManager.swift
@@ -23,8 +23,6 @@ class RoutineManager {
   }
   
   func addPlan(with workouts: [PlannedWorkout], on dateInformation: DateInformation) {
-    workoutPlanner[dateInformation] =  plan(of: dateInformation) + workouts
-    
     let itemRef = configureDatabaseReference(dateInformation: dateInformation)
     
     for workout in workouts {
@@ -41,32 +39,29 @@ class RoutineManager {
         print(error)
       }
     }
+    
+    workoutPlanner[dateInformation] =  plan(of: dateInformation) + workouts
   }
   
-  func removeWorkout(workout: PlannedWorkout, on dateInformation: DateInformation) {
-    guard let id = workout.id else { return }
-    
-    let itemRef = configureDatabaseReference(dateInformation: dateInformation)
-    itemRef.child("/\(id)").removeValue()
+  func reorderPlan(on date: DateInformation, removeAt removingPosition: Int, insertAt insertingPosition: Int) {
+    var reorderingPlan = self.plan(of: date)
+    let reorderingWorkout = reorderingPlan[removingPosition]
+    reorderingPlan.remove(at: removingPosition)
+    reorderingPlan.insert(reorderingWorkout, at: insertingPosition)
+    workoutPlanner[date] = reorderingPlan
+    self.updatePlan(with: reorderingPlan, on: date)
   }
-//  func updatePlan(with workouts: [PlannedWorkout], on dateInformation: DateInformation) {
-//    guard workoutPlanner[dateInformation] != nil else {
-//      return
-//    }
-//    
-//    for workout in workouts {
-//      do {
-//        let data = try encoder.encode(workout)
-//        let json = try JSONSerialization.jsonObject(with: data)
-//        
-//        guard let id = workout.id else { return }
-//        let childUpdates = ["/routine/\(dateInformation.currentDate)/\(id)/": json]
-//        self.ref.updateChildValues(childUpdates)
-//      } catch {
-//        print(error)
-//      }
-//    }
-//  }
+  
+  func updatePlan(with workouts: [PlannedWorkout], on dateInformation: DateInformation) {
+    guard workoutPlanner[dateInformation] != nil else {
+      return
+    }
+    
+    for (idx, workout) in workouts.enumerated() {
+      workout.sequenceNumber = UInt(idx)
+      updateWorkout(workout: workout, on: dateInformation)
+    }
+  }
   
   func updateWorkout(workout: PlannedWorkout, on dateInformation: DateInformation) {
     do {
@@ -78,6 +73,19 @@ class RoutineManager {
     } catch {
       print(error)
     }
+  }
+  
+  func removeWorkout(at removingPosition: Int, on dateInformation: DateInformation) {
+    var reorderingPlan = self.plan(of: dateInformation)
+    let removingWorkout = reorderingPlan[removingPosition]
+    reorderingPlan.remove(at: removingPosition)
+    workoutPlanner[dateInformation] = reorderingPlan
+    
+    guard let id = removingWorkout.id else { return }
+    let itemRef = configureDatabaseReference(dateInformation: dateInformation)
+    itemRef.child("/\(id)").removeValue()
+    
+    self.updatePlan(with: reorderingPlan, on: dateInformation)
   }
   
   private func configureDatabaseReference(dateInformation dateInfo: DateInformation) -> DatabaseReference {
