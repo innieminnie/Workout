cecf3bdf280529f1d158d664779de2b36df2769f
innieminnie <rkddlsl1001@gmail.com>
2022-11-01 16:21:51 +0900

    fix: WorkoutTableViewCell의 UILabel 글자잘림 수정

--- Workout/CustomCells/WorkoutTableViewCell.swift
+++ Workout/CustomCells/WorkoutTableViewCell.swift
@@ -9,16 +9,17 @@ import UIKit
 
 class WorkoutTableViewCell: UITableViewCell {
   static let identifier = "workoutTableViewCell"
-  @IBOutlet weak var workoutNameLabel: UILabel!
-
+  @IBOutlet weak var containerView: UIView!
+  @IBOutlet weak var workoutNameLabel: PaddingLabel!
+  
   override func awakeFromNib() {
     super.awakeFromNib()
-    contentView.layer.cornerRadius = 13
+    containerView.applyShadow()
   }
   
   override func layoutSubviews() {
     super.layoutSubviews()
-    contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10))
+    contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5))
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
--- Workout/Views/Extensions.swift
+++ Workout/Views/Extensions.swift
@@ -11,7 +11,7 @@ extension UIView {
   func applyShadow() {
     self.layer.cornerRadius = 8
     self.layer.shadowColor = UIColor.gray.cgColor
-    self.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
+    self.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
     self.layer.shadowOpacity = 0.3
     self.layer.masksToBounds = false
   }
