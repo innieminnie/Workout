2a7e4078b7181ac2884dbe23dfaebf7acc9a9290
innieminnie <rkddlsl1001@gmail.com>
2022-11-17 02:21:34 +0900

    fix: completSignInProcess 호출 위치 수정

--- Workout/ViewControllers/SignInViewController.swift
+++ Workout/ViewControllers/SignInViewController.swift
@@ -58,7 +58,6 @@ class SignInViewController: UIViewController {
   
   @objc private func tappedGoogleSignInButton() {
     AuthenticationManager.shared.googleLoginProcess(presentingVC: self)
-    self.dismiss(animated: true)
   }
   
   @objc private func tappedAppleSignInButton() {
@@ -74,12 +73,16 @@ class SignInViewController: UIViewController {
     authorizationController.presentationContextProvider = self
     
     AuthenticationManager.shared.appleLoginProcess(with: authorizationController)
-    self.dismiss(animated: true)
   }
   
   @objc private func tappedKakaoSignInButton() {
-    AuthenticationManager.shared.kakaoLoginProcess()
-    self.dismiss(animated: true)
+    AuthenticationManager.shared.kakaoLoginProcess(presentingVC: self)
+  }
+  
+  func completeSignInProcess() {
+    let baseTabBarController = BaseTabBarController()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    appDelegate.changeRootViewController(baseTabBarController, animated: true)
   }
   
   private func sha256(_ input: String) -> String {
@@ -88,17 +91,17 @@ class SignInViewController: UIViewController {
     let hashString = hashedData.compactMap {
       String(format: "%02x", $0)
     }.joined()
-
+    
     return hashString
   }
   
   private func randomNonceString(length: Int = 32) -> String {
     precondition(length > 0)
     let charset: [Character] =
-      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+    Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
     var result = ""
     var remainingLength = length
-
+    
     while remainingLength > 0 {
       let randoms: [UInt8] = (0 ..< 16).map { _ in
         var random: UInt8 = 0
@@ -110,19 +113,19 @@ class SignInViewController: UIViewController {
         }
         return random
       }
-
+      
       randoms.forEach { random in
         if remainingLength == 0 {
           return
         }
-
+        
         if random < charset.count {
           result.append(charset[Int(random)])
           remainingLength -= 1
         }
       }
     }
-
+    
     return result
   }
 }
@@ -133,34 +136,34 @@ extension SignInViewController: ASAuthorizationControllerPresentationContextProv
   
   func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
     if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
-          guard let nonce = currentNonce else {
-            fatalError("Invalid state: A login callback was received, but no login request was sent.")
-          }
-          guard let appleIDToken = appleIDCredential.identityToken else {
-            print("Unable to fetch identity token")
-            return
-          }
-          guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
-            return
-          }
-          // Initialize a Firebase credential.
-          let credential = OAuthProvider.credential(withProviderID: "apple.com",
-                                                    idToken: idTokenString,
-                                                    rawNonce: nonce)
-          // Sign in with Firebase.
-          Auth.auth().signIn(with: credential) { (authResult, error) in
-            if let error = error {
-              // Error. If error.code == .MissingOrInvalidNonce, make sure
-              // you're sending the SHA256-hashed nonce as a hex string with
-              // your request to Apple.
-              print(error.localizedDescription)
-              return
-            }
-            // User is signed in to Firebase with Apple.
-            // ...
-          }
+      guard let nonce = currentNonce else {
+        fatalError("Invalid state: A login callback was received, but no login request was sent.")
+      }
+      guard let appleIDToken = appleIDCredential.identityToken else {
+        print("Unable to fetch identity token")
+        return
+      }
+      guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+        print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+        return
+      }
+      // Initialize a Firebase credential.
+      let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                idToken: idTokenString,
+                                                rawNonce: nonce)
+      // Sign in with Firebase.
+      Auth.auth().signIn(with: credential) { (authResult, error) in
+        if let error = error {
+          // Error. If error.code == .MissingOrInvalidNonce, make sure
+          // you're sending the SHA256-hashed nonce as a hex string with
+          // your request to Apple.
+          print(error.localizedDescription)
+          return
         }
+        // User is signed in to Firebase with Apple.
+        self.completeSignInProcess()
+      }
+    }
   }
   
   func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
