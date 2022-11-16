3b4dc487c4865afc87e9f7a30f0644651d7caab5
innieminnie <rkddlsl1001@gmail.com>
2022-11-17 02:21:34 +0900

    fix: completSignInProcess 호출 위치 수정

--- Workout/Models/AuthenticationManager.swift
+++ Workout/Models/AuthenticationManager.swift
@@ -15,10 +15,10 @@ import AuthenticationServices
 class AuthenticationManager {
   static let shared = AuthenticationManager()
   static let user = Auth.auth().currentUser
-
+  
   private init() { }
   
-  func googleLoginProcess(presentingVC: UIViewController) {
+  func googleLoginProcess(presentingVC: SignInViewController) {
     guard let clientID = APIKey().googleClientID else { return }
     let config = GIDConfiguration.init(clientID: clientID)
     
@@ -32,31 +32,35 @@ class AuthenticationManager {
       let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
       Auth.auth().signIn(with: credential) { autoDataResult, error in
         if let error = error { print(error) }
+        else {
+          presentingVC.completeSignInProcess()
+        }
       }
     }
   }
+  
   
   func appleLoginProcess(with authorizationController: ASAuthorizationController) {
     authorizationController.performRequests()
   }
   
-  func kakaoLoginProcess() {
+  func kakaoLoginProcess(presentingVC: SignInViewController) {
     if (UserApi.isKakaoTalkLoginAvailable()) {
       UserApi.shared.loginWithKakaoTalk { oauthToken, error in
-        self.onKakaoLoginCompleted(oauthToken?.accessToken)
+        self.onKakaoLoginCompleted(oauthToken?.accessToken, presentingVC)
       }
     } else {
       UserApi.shared.loginWithKakaoAccount { oauthToken, error in
-        self.onKakaoLoginCompleted(oauthToken?.accessToken)
+        self.onKakaoLoginCompleted(oauthToken?.accessToken, presentingVC)
       }
     }
   }
   
-  func onKakaoLoginCompleted(_ accessToken: String?) {
-    connectFirebaseAuthentication(accessToken)
+  func onKakaoLoginCompleted(_ accessToken: String?,_ presentingVC: SignInViewController) {
+    connectFirebaseAuthentication(accessToken, presentingVC)
   }
   
-  func connectFirebaseAuthentication(_ accessToken: String?) {
+  func connectFirebaseAuthentication(_ accessToken: String?, _ presentingVC: SignInViewController) {
     UserApi.shared.me() { user, error in
       if error == nil {
         guard let user = user, let kakaoEmail = user.kakaoAccount?.email, let id = user.id else {
@@ -66,6 +70,7 @@ class AuthenticationManager {
         Auth.auth().createUser(withEmail: kakaoEmail, password: String(describing: id)) { autoDataResult, error in
           if error != nil {
             Auth.auth().signIn(withEmail: kakaoEmail, password: String(describing: id), completion: nil)
+            presentingVC.completeSignInProcess()
           }
         }
       }
