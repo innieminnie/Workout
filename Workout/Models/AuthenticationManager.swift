91808afa7d45bfb1781a5af7a1d19f8568a31805
innieminnie <rkddlsl1001@gmail.com>
2022-11-26 19:03:20 +0900

    feat: 이미 가입된 아이디 있는 경우 에러핸들링 추가

--- Workout/Models/AuthenticationManager.swift
+++ Workout/Models/AuthenticationManager.swift
@@ -75,7 +75,18 @@ class AuthenticationManager {
         
         Auth.auth().createUser(withEmail: kakaoEmail, password: String(describing: id)) { autoDataResult, error in
           if error != nil {
-            Auth.auth().signIn(withEmail: kakaoEmail, password: String(describing: id), completion: nil)
+            let errorCode = AuthErrorCode(rawValue: error!._code)
+            switch errorCode {
+            case .emailAlreadyInUse:
+              let alert = UIAlertController(title: "다른 로그인 방식으로 해당 이메일 계정을 사용한 기록이 있어요.\n\n 다른 로그인 방식을 시도해주세요!", message: nil, preferredStyle: .alert)
+              let action = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+              alert.addAction(action)
+              presentingVC.present(alert, animated: false, completion: nil)
+              return
+              
+            default:
+              Auth.auth().signIn(withEmail: kakaoEmail, password: String(describing: id), completion: nil)
+            }
           }
           
           presentingVC.completeSignInProcess()
