36936a0fd751d972ada3a3f776e417bf7d79e011
innieminnie <rkddlsl1001@gmail.com>
2022-12-13 20:47:42 +0900

    chores: HomeVC 코드 정리

--- Workout/ViewControllers/HomeViewController.swift
+++ Workout/ViewControllers/HomeViewController.swift
@@ -45,7 +45,6 @@ class HomeViewController: UIViewController {
   }()
   
   private let routineTableView = RoutineTableView()
-  
   private var editableField: UITextField?
   private var handle: AuthStateDidChangeListenerHandle?
   
@@ -79,24 +78,6 @@ class HomeViewController: UIViewController {
     Auth.auth().removeStateDidChangeListener(handle!)
   }
   
-  @objc  private func checkRoutineData(_ notification: NSNotification) {
-    guard let userInfo = notification.userInfo, let dateInformation = userInfo["date"] as? DateInformation else { return }
-    
-    if selectedDayInformation == dateInformation {
-      if let error = notification.userInfo?["error"] as? Error {
-        let alert = UIAlertController(title: "\(error)\n데이터 읽기에 실패했어요.\n잠시후 다시 시도해주세요.", message: nil, preferredStyle: .alert)
-        let action = UIAlertAction(title: "확인", style: .destructive, handler: nil)
-        alert.addAction(action)
-        self.present(alert, animated: false, completion: nil)
-        return
-      }
-      
-      DispatchQueue.main.async {
-        self.routineTableView.reloadData()
-      }
-    }
-  }
-  
   @objc private func tappedAddRoutineButton(sender: UIButton) {
     hideKeyboard()
     
@@ -104,129 +85,6 @@ class HomeViewController: UIViewController {
     routineSelectionViewController.delegate = self
     routineSelectionViewController.modalPresentationStyle = .formSheet
     self.present(routineSelectionViewController, animated: true, completion: nil)
-  }
-  
-  @objc private func trackTappedTextField(notification: Notification) {
-    if let activatedField = notification.object as? UITextField {
-      self.editableField = activatedField
-      activatedField.selectAll(nil)
-    }
-  }
-  
-  @objc private func hideKeyboard() {
-    if let activatedField = editableField {
-      activatedField.resignFirstResponder()
-    }
-  }
-  
-  @objc private func keyboardWillShow(notification: NSNotification) {
-    guard let userInfo = notification.userInfo,
-          let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
-      return
-    }
-    
-    let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height, right: 0.0)
-    contentScrollView.contentInset = contentInsets
-    contentScrollView.verticalScrollIndicatorInsets = contentInsets
-    
-    if let activeField = editableField {
-      let activeRect = activeField.convert(activeField.bounds, to: contentScrollView)
-      contentScrollView.scrollRectToVisible(activeRect, animated: true)
-    }
-  }
-  
-  @objc private func updateCalendar(_ notification: NSNotification) {
-    if let userInfo = notification.userInfo,
-       let error = userInfo["error"] as? Error {
-      let alert = UIAlertController(title: "\(error)\n데이터 읽기에 실패했어요.\n잠시후 다시 시도해주세요.", message: nil, preferredStyle: .alert)
-      let action = UIAlertAction(title: "확인", style: .destructive, handler: nil)
-      alert.addAction(action)
-      self.present(alert, animated: false, completion: nil)
-      return
-    } else {
-      self.calendarView.reloadUserData()
-    }
-  }
-  
-  private func updateTableView() {
-    routineTableView.beginUpdates()
-    routineTableView.endUpdates()
-  }
-}
-
-extension HomeViewController {
-  private func configureNotification() {
-    NotificationCenter.default.addObserver(self, selector: #selector(trackTappedTextField), name: NSNotification.Name("TappedTextField"), object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardDidShowNotification, object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(self.checkRoutineData(_:)), name: Notification.Name("ReadRoutineData"), object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(self.updateCalendar(_:)), name: Notification.Name("ReadWorkoutData"), object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(hideKeyboard), name: Notification.Name("CheckKeyboard"), object: nil)
-  }
-  
-  private func configureGestureRecognizer() {
-    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-    tapGestureRecognizer.numberOfTapsRequired = 1
-    tapGestureRecognizer.isEnabled = true
-    contentScrollView.addGestureRecognizer(tapGestureRecognizer)
-  }
-  
-  private func configureAuthListener() {
-    handle = Auth.auth().addStateDidChangeListener { auth, user in
-      guard let user = user, let email = user.email else { return }
-      
-      AuthenticationManager.shared.setAccountInformation(email)
-      
-      if currentUser == nil {
-        HomeViewController.signedUpUser = user.uid
-      }
-      
-      workoutManager.readWorkoutData()
-    }
-  }
-  
-  private func setUpLayout() {
-    NSLayoutConstraint.activate([
-      contentScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-      contentScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
-      contentScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
-      contentScrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
-      
-      calendarView.topAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.topAnchor, constant: 10),
-      calendarView.leadingAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.leadingAnchor),
-      calendarView.trailingAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.trailingAnchor),
-      calendarView.widthAnchor.constraint(equalTo: contentScrollView.frameLayoutGuide.widthAnchor),
-      
-      addRoutineButton.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 10),
-      addRoutineButton.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor, constant: 10),
-      addRoutineButton.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor, constant: -10),
-      
-      routineTableView.topAnchor.constraint(equalTo: addRoutineButton.bottomAnchor, constant: 10),
-      routineTableView.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor),
-      routineTableView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor),
-      routineTableView.bottomAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.bottomAnchor, constant: -50),
-    ])
-  }
-}
-extension HomeViewController: RoutineSelectionDelegate {
-  func addSelectedWorkouts(_ selectedWorkouts: [Workout]) {
-    guard let selectedDayInformation = self.selectedDayInformation else { return }
-    
-    let plannedWorkoutNumber = routineManager.plan(of: selectedDayInformation).count
-    let newPlannedWorkouts = selectedWorkouts.enumerated().map({ (index, workout) in
-      PlannedWorkout(workout, UInt(index + plannedWorkoutNumber), selectedDayInformation)
-    })
-    
-    routineManager.addPlan(with: newPlannedWorkouts, on: selectedDayInformation)
-    calendarView.updateSelectedCell()
-    
-    routineTableView.beginUpdates()
-    for i in plannedWorkoutNumber..<routineManager.plan(of: selectedDayInformation).count {
-      self.routineTableView.insertRows(at: [IndexPath(item: i, section: 0)], with: .right)
-    }
-    routineTableView.layoutIfNeeded()
-    routineTableView.endUpdates()
-    
-    contentScrollView.scrollToBottom()
   }
 }
 extension HomeViewController: UITableViewDataSource {
@@ -259,8 +117,8 @@ extension HomeViewController: UITableViewDataSource {
   
   func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
     guard sourceIndexPath.row != destinationIndexPath.row else { return }
-    
     guard let selectedDayInformation = self.selectedDayInformation else { return }
+    
     routineManager.reorderPlan(on: selectedDayInformation, removeAt: sourceIndexPath.row, insertAt: destinationIndexPath.row)
   }
 }
@@ -279,13 +137,47 @@ extension HomeViewController: UITableViewDelegate {
     }
   }
 }
-extension HomeViewController: TabBarMenu {
-  var tabTitle: String {
-    return "홈"
+extension HomeViewController: UITableViewDragDelegate {
+  func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+    return [UIDragItem(itemProvider: NSItemProvider())]
+  }
+}
+extension HomeViewController: UITableViewDropDelegate {
+  func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
   }
   
-  var icon: String {
-    return "house.fill"
+  func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+    if session.localDragSession != nil {
+      return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+    }
+    return UITableViewDropProposal(operation: .cancel, intent: .unspecified)
+  }
+}
+extension HomeViewController: RoutineSelectionDelegate {
+  func addSelectedWorkouts(_ selectedWorkouts: [Workout]) {
+    guard let selectedDayInformation = self.selectedDayInformation else { return }
+    
+    let plannedWorkoutNumber = routineManager.plan(of: selectedDayInformation).count
+    let newPlannedWorkouts = selectedWorkouts.enumerated().map({ (index, workout) in
+      PlannedWorkout(workout, UInt(index + plannedWorkoutNumber), selectedDayInformation)
+    })
+    
+    routineManager.addPlan(with: newPlannedWorkouts, on: selectedDayInformation)
+    calendarView.updateSelectedCell()
+    
+    routineTableView.beginUpdates()
+    for i in plannedWorkoutNumber..<routineManager.plan(of: selectedDayInformation).count {
+      self.routineTableView.insertRows(at: [IndexPath(item: i, section: 0)], with: .right)
+    }
+    routineTableView.layoutIfNeeded()
+    routineTableView.endUpdates()
+    
+    contentScrollView.scrollToBottom()
+  }
+}
+extension HomeViewController: CalendarViewDelegate {
+  func changedSelectedDay(to dateInformation: DateInformation?) {
+    self.selectedDayInformation = dateInformation
   }
 }
 extension HomeViewController: WorkoutPlanCardTableViewCellDelegate {
@@ -308,24 +200,130 @@ extension HomeViewController: WorkoutPlanCardTableViewCellDelegate {
     self.present(alert, animated: false, completion: nil)
   }
 }
-extension HomeViewController: UITableViewDragDelegate {
-  func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-    return [UIDragItem(itemProvider: NSItemProvider())]
-  }
-}
-extension HomeViewController: UITableViewDropDelegate {
-  func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+extension HomeViewController: TabBarMenu {
+  var tabTitle: String {
+    return "홈"
   }
   
-  func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
-    if session.localDragSession != nil {
-      return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
-    }
-    return UITableViewDropProposal(operation: .cancel, intent: .unspecified)
+  var icon: String {
+    return "house.fill"
   }
 }
-extension HomeViewController: CalendarViewDelegate {
-  func changedSelectedDay(to dateInformation: DateInformation?) {
-    self.selectedDayInformation = dateInformation
+extension HomeViewController {
+  private func configureNotification() {
+    NotificationCenter.default.addObserver(self, selector: #selector(trackTappedTextField), name: NSNotification.Name("TappedTextField"), object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(self.checkRoutineData(_:)), name: Notification.Name("ReadRoutineData"), object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(self.updateCalendar(_:)), name: Notification.Name("ReadWorkoutData"), object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(hideKeyboard), name: Notification.Name("CheckKeyboard"), object: nil)
+  }
+  
+  @objc private func trackTappedTextField(notification: Notification) {
+    if let activatedField = notification.object as? UITextField {
+      self.editableField = activatedField
+      activatedField.selectAll(nil)
+    }
+  }
+  
+  @objc private func keyboardWillShow(notification: NSNotification) {
+    guard let userInfo = notification.userInfo,
+          let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+      return
+    }
+    
+    let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height, right: 0.0)
+    contentScrollView.contentInset = contentInsets
+    contentScrollView.verticalScrollIndicatorInsets = contentInsets
+    
+    if let activeField = editableField {
+      let activeRect = activeField.convert(activeField.bounds, to: contentScrollView)
+      contentScrollView.scrollRectToVisible(activeRect, animated: true)
+    }
+  }
+  
+  @objc  private func checkRoutineData(_ notification: NSNotification) {
+    guard let userInfo = notification.userInfo, let dateInformation = userInfo["date"] as? DateInformation else { return }
+    
+    if selectedDayInformation == dateInformation {
+      if let error = notification.userInfo?["error"] as? Error {
+        let alert = UIAlertController(title: "\(error)\n데이터 읽기에 실패했어요.\n잠시후 다시 시도해주세요.", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: false, completion: nil)
+        return
+      }
+      
+      DispatchQueue.main.async {
+        self.routineTableView.reloadData()
+      }
+    }
+  }
+
+  @objc private func updateCalendar(_ notification: NSNotification) {
+    if let userInfo = notification.userInfo,
+       let error = userInfo["error"] as? Error {
+      let alert = UIAlertController(title: "\(error)\n데이터 읽기에 실패했어요.\n잠시후 다시 시도해주세요.", message: nil, preferredStyle: .alert)
+      let action = UIAlertAction(title: "확인", style: .destructive, handler: nil)
+      alert.addAction(action)
+      self.present(alert, animated: false, completion: nil)
+      return
+    } else {
+      self.calendarView.reloadUserData()
+    }
+  }
+  
+  @objc private func hideKeyboard() {
+    if let activatedField = editableField {
+      activatedField.resignFirstResponder()
+    }
+  }
+  
+  private func configureGestureRecognizer() {
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+    tapGestureRecognizer.numberOfTapsRequired = 1
+    tapGestureRecognizer.isEnabled = true
+    contentScrollView.addGestureRecognizer(tapGestureRecognizer)
+  }
+  
+  private func configureAuthListener() {
+    handle = Auth.auth().addStateDidChangeListener { auth, user in
+      guard let user = user, let email = user.email else { return }
+      
+      AuthenticationManager.shared.setAccountInformation(email)
+      
+      if currentUser == nil {
+        HomeViewController.signedUpUser = user.uid
+      }
+      
+      workoutManager.readWorkoutData()
+    }
+  }
+  
+  private func updateTableView() {
+    routineTableView.beginUpdates()
+    routineTableView.endUpdates()
+  }
+  
+  private func setUpLayout() {
+    NSLayoutConstraint.activate([
+      contentScrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+      contentScrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+      contentScrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+      contentScrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+      
+      calendarView.topAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.topAnchor, constant: 10),
+      calendarView.leadingAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.leadingAnchor),
+      calendarView.trailingAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.trailingAnchor),
+      calendarView.widthAnchor.constraint(equalTo: contentScrollView.frameLayoutGuide.widthAnchor),
+      
+      addRoutineButton.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 10),
+      addRoutineButton.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor, constant: 10),
+      addRoutineButton.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor, constant: -10),
+      
+      routineTableView.topAnchor.constraint(equalTo: addRoutineButton.bottomAnchor, constant: 10),
+      routineTableView.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor),
+      routineTableView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor),
+      routineTableView.bottomAnchor.constraint(equalTo: contentScrollView.contentLayoutGuide.bottomAnchor, constant: -50),
+    ])
   }
 }
