e95bb4f644da02c1393f4f1834a9abf90cdd8bbe
innieminnie <rkddlsl1001@gmail.com>
2023-01-11 00:30:48 +0900

    chores: 파일 정리

---
+++ Workout/SupportingFiles/APIKey.swift
@@ -0,0 +1,17 @@
+//
+//  APIKey.swift
+//  Workout
+//
+//  Created by 강인희 on 2022/11/15.
+//
+
+import Foundation
+import Firebase
+struct APIKey {
+  let kakaoNativeAppKey = "4983771287729e3a63a0b0ef1c07f1c9"
+  let googleClientID = FirebaseApp.app()?.options.clientID
+}
+
+// 주황: 0xF58423.convertToRGB()
+// 남색: 0x096DB6.convertToRGB()
+// 회색: 0xBEC0C2.convertToRGB()
